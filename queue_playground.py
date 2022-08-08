16c6686dbd6972ea93791edbcb55bb17edb12454
Hayden Jones <haydenjones@protonmail.com>
2022-08-08 14:38:36 -0400

    i tried my best at jotto. the "evolve" function in theory is guaranteed to eventually find the solution, but I ran it overnight and didn't get it.

---
+++ queue_playground.py
@@ -0,0 +1,14 @@
+import random
+from dataclasses import dataclass
+
+s = "everyone should have food water shelter and healthcare"
+
+print("".join([f" :sm64_{c}:" for c in s if c in "abcdefghijklmnopqrstuvwxyz"]))
+
+def zz():
+    res = []
+    for x in range(100):
+        res.append(f":zz{(str(x).zfill(2))}: ")
+    print("".join(res))
+
+#zz()
